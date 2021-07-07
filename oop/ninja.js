a95d15bb59cfcdacd62fa6d1f002d394ad08fb26
longnghts <82634370+longnghts@users.noreply.github.com>
2021-07-07 18:43:48 -0400

    updated for Super Ninja

--- oop/ninja.js
+++ oop/ninja.js
@@ -29,7 +29,22 @@ class Ninja{
     }
 }
 
+class Sensei extends Ninja{
+    constructor(name, health){
+        super(name, health);
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        this.drinkSake();
+        console.log('These are the words of wisdom that i will speak so sweetly to you.');
+        }
+    }
+
+
 const ninja1 = new Ninja("Hyabusa", 300);
+const sensei1 = new Sensei("Mike", 9001);
+
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+sensei1.speakWisdom();
